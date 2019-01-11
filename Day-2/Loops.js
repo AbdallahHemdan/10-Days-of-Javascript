0a3cefa8d5b6a3b7b24b44963af61bbdd6d82ece
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-11 04:46:20 +0200

    Create Loops.js

---
+++ Day-2/Loops.js
@@ -0,0 +1,81 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function vowelsAndConsonants(s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            console.log(s[i]);
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u')
+            console.log(s[i]);
+    }
+}
+
+
+function main() {
+    const s = readLine();
+
+    vowelsAndConsonants(s);
+}
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function vowelsAndConsonants(s) {
+    for (let i = 0; i < s.length; i++){
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            console.log(s[i]);
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u')
+            console.log(s[i]);
+    }
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
